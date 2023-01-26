359ccc998375fe4439c0d720ea78c3ef2e5348bc
Gathin <gathint23@gmail.com>
2023-01-26 11:25:47 +0530

    WalletConnect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import {useState} from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
 function App() {
+
+  const [walletAddress, setWalletAddress] = useState("")
+  
+  
+  async function connectWallet() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await window.ethereum.request({method:"eth_requestAccounts"})
+    setWalletAddress(accounts[0])
+    const signer = provider.getSigner();
+    const balance = await signer.getBalance();
+    console.log(ethers.utils.formatEther(balance))
+    }
+
+
   return (
-    <div>
-      
+    <div className='Wallet'>
+      <div>
+        <button onClick={connectWallet}>Connect Wallet</button>
+        <h3>Address: {walletAddress}</h3>
+      </div>
+       
     </div>
   );
 }
