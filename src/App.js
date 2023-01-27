681be4878b93790fbb05602e67f17d2889a3609e
Gathin <gathint23@gmail.com>
2023-01-27 08:59:03 +0530

    Tokenaddress

--- src/App.js
+++ src/App.js
@@ -6,23 +6,34 @@ import contractJson from './contracts/ERC20Factory.json';
 function App() {
 
   const [walletAddress, setWalletAddress] = useState("")
+  const [signer, setSigner] = useState()
+  const [contract, setContract] = useState()
+  const [token1Address, settoken1Address] = useState("")
+  const [token2Address, settoken2Address] = useState("")
+  const [token1Balance, settoken1Balance] = useState("")
+
   
   
   async function connectWallet() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const accounts = await window.ethereum.request({method:"eth_requestAccounts"})
-    setWalletAddress(accounts[0])
+    await setWalletAddress(accounts[0])
     const signer = provider.getSigner();
+    setSigner(signer);
 
-    const contractAddress="0xFF3EeEB33FA028EAa7cbcd518AE01563ce81717A"
+    const contractAddress="0x0e2F3f475800302C6b024669009eDb1F58f44f93"
     const contract = new ethers.Contract(contractAddress,contractJson.abi,signer)
-
+    setContract(contract)
     const token1Address = await contract.getAddressForSymbol("TK1")
     const token2Address = await contract.getAddressForSymbol("TK2")
+    settoken1Address(token1Address)
+    settoken2Address(token2Address)
+    
+  }
 
- 
-
- }
+  async function tokenBalance() {
+    
+  }
     
 
 
@@ -32,8 +43,11 @@ function App() {
         <button onClick={connectWallet}>Connect Wallet</button>
         <h3>Address: {walletAddress}</h3>
       </div>
-      <div className='token'>
-        
+      <div className='token'> 
+        <button onClick={tokenBalance}>Balance</button>
+        <h3>The current token balance in your account are:</h3>
+        <h4>TK1 Balance: </h4>
+        <h4>TK2 Balance: </h4>
       </div>
        
     </div>
